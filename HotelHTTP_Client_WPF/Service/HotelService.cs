df04d24a3a194adfc46f36e06ae2a2a4162a6c3d
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-15 17:41:48 +0300

    Add HotelService
    -Async method for geting avaible rooms.
    -Async method for booking room.

---
+++ HotelHTTP_Client_WPF/Service/HotelService.cs
@@ -0,0 +1,48 @@
+﻿using HotelHTTP_Client_WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelHTTP_Client_WPF.Service
+{
+    class HotelService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public HotelService() 
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:5001/");
+        }
+
+        public async Task<List<Room>> GetAvaibleRoomsAsync()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<List<Room>>("api/avaible");
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show($"Помилка при отриманні номерів: {ex.Message}");
+                return new List<Room>();
+            }
+        }
+
+        public async Task<bool> BookRoomAsync(int roomId, DateTime startTime, DateTime endTime)
+        {
+            var booking = new
+            {
+                RoomId = roomId,
+                StartTime = startTime,
+                EndTime = endTime
+            };
+
+            var responce = await _httpClient.PostAsJsonAsync("api/booking", booking);
+            return responce.IsSuccessStatusCode;
+        }
+    }
+}
