d2108a28d9c63e787f0cda213e61a595970296e0
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-15 17:28:03 +0300

    Init Model layer
    -Add Room Entity.

---
+++ HotelHTTP_Client_WPF/Model/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHTTP_Client_WPF.Model
+{
+    class Room
+    {
+        public int Id { get; set; }
+        public int Beds { get; set; }
+        public int PricePerDay { get; set; }
+        bool IsAvaible { get; set; }
+    }
+}
