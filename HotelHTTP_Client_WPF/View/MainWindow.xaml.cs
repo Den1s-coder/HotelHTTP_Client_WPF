877d84bb0d92c4142dbaac34b3c42d57bc46ed66
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-16 17:16:48 +0300

    Add UI (#3)
    -ADD buttons for Load JSON file with rooms info.
    -ADD button to select a booking room.
    -ADD a DataGrid that displays room information.

--- HotelHTTP_Client_WPF/View/MainWindow.xaml.cs
+++ HotelHTTP_Client_WPF/View/MainWindow.xaml.cs
@@ -11,9 +11,6 @@ using System.Windows.Shapes;
 
 namespace HotelHTTP_Client_WPF.View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
